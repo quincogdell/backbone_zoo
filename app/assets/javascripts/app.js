f2d546ca4ba6bafdb9efdf0b3762849afc829ada
Quin Cogdell <quincogdell@gmail.com>
2013-11-25 14:58:48 -0500

    stping point before data hash

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,56 +1,36 @@
 var ZooView = Backbone.View.extend({
-  // corresponding DOM element
   el: "#zoo",
 
-  // gets called with new ZooView
-  initialize: function(){
-    console.log("making a  zoo view");
-  },
-
   events: {
-    // type of event : function to call
-    "click": "welcome",
-    // delegates events to other DOM elements in the view
-    "mouseover section": "highlight"
+    "click button": "makeAnimal"
+  },
+  initialize: function(data){
+    this.data = data;
   },
 
-  highlight: function(e){
-    // the event
-    console.log(e);
-    // the view object
-    console.log(this);
-    this.$el.find('section').css({backgroundColor: 'yellow'});
-  },
-
-  welcome: function(e){
-    alert("welcome to the zoo");
-  }
-
-});
-
-var JungleView = Backbone.View.extend({
-  id: "jungle",
-  initialize: function(){
-    console.log("making a jungle view");
-    $('body').append(this.render().el);
-  },
-  // events hash
-  events: {
-    "click": "watchOut"
-  },
-
-  render: function() {
-    this.$el.text("Welcome to the Jungle");
-    return this;
-  },
-  watchOut: function() {
-    this.$el.text("Watch out for the tree");
-    return this;
+  makeAnimal: function(e){
+    console.log("Making Animal");
+    var animalView = new AnimalView();
+    // appending new Animal liTag.
+    $("#animals").append(animalView.el);
   }
 });
 
+var AnimalView = Backbone.View.extend({
+  // overriding defaults
+  tagName: "li",
+  className: "animal",
+  initialize: function(){
+    this.$el.text("Animal");
+  },
+  events: {
+    "click": "makeRed"
+  },
+  makeRed: function(){
+    this.$el.css({background: "red"});
+  }
+});
 
 $(document).ready(function(){
-  window.zooView = new ZooView();
-  window.jungleView = new JungleView();
+  var zooView = new ZooView();
 });
